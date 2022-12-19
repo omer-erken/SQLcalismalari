3c58654b078fe0b997eb5ee82b3ee8435c2546d6
omer-erken <118456858+omer-erken@users.noreply.github.com>
2022-12-19 14:59:07 +0300

    Add files via upload

--- Odev_1.sql
+++ Odev_1.sql
@@ -9,4 +9,4 @@ SELECT title, description FROM film;
 SELECT * FROM film WHERE lenght>60 AND lenght<75
 SELECT * FROM film WHERE rental_rate =0.99 AND replacement_cost=12.99 OR replacement_cost= 28.99;
 SELECT last_name FROM customer WHERE first_name='Mary';
-SELECT title FROM film WHERE NOT (filmLengt=50 AND rental_rate=2.99 OR rental_rate=4.99)
+SELECT title FROM film WHERE NOT (lengt=50 AND rental_rate=2.99 OR rental_rate=4.99)
