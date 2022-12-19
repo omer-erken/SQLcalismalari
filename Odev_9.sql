d27cc234cd78742d4c3158f6f7c4dfa2f9ffdaf0
omer-erken <118456858+omer-erken@users.noreply.github.com>
2022-12-19 18:44:46 +0300

    Add files via upload

---
+++ Odev_9.sql
@@ -0,0 +1,18 @@
+/*
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name 
+isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name
+isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+*/
+
+SELECT city,country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+SELECT payment_id,first_name,last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
+
+SELECT rental_id,first_name,last_name FROM rental
+INNER JOIN customer ON customer.customer_id = rental.customer_id;
